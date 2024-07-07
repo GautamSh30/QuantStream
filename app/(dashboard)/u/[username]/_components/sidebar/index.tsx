fec3c0245226804976f0310b99900465958f3cc4
Gautam Sharma <gautam2586272@gmail.com>
2024-07-07 18:16:25 +0530

    creator dashboard

---
+++ app/(dashboard)/u/[username]/_components/sidebar/index.tsx
@@ -0,0 +1,13 @@
+import { useCreatorSidebar } from "@/store/use-creator-sidebar";
+import { Toggle } from "./toggle";
+import { Wrapper } from "./wrapper";
+import { Navigation } from "./navigation";
+
+export const Sidebar = () => {
+  return (
+    <Wrapper>
+      <Toggle />
+      <Navigation />
+    </Wrapper>
+  );
+};
