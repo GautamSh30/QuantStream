677f09c2e46e6bd646288b27e8bb478248f0b27b
Gautam Sharma <gautam2586272@gmail.com>
2024-07-07 12:21:50 +0530

    model update

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,11 +1,31 @@
 import { db } from "./db";
 import { getSelf } from "./auth-service";
+
 export const getRecommended = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  let userId;
+
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch (error) {
+    userId = null;
+  }
+
+  let users = [];
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId,
+        },
+      },
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
   return users;
 };
