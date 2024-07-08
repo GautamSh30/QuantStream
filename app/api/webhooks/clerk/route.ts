5ca015e5fa722d1871fec398a6efd7e4d472880d
Gautam Sharma <gautam2586272@gmail.com>
2024-07-08 11:07:57 +0530

    stream and dashboard-chat settings

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,33 +2,40 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
+
 export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      "Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
   }
 
+  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
       status: 400,
     });
   }
 
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -42,29 +49,33 @@ export async function POST(req: Request) {
     });
   }
 
+  // Get the ID and type
+  const { id } = evt.data;
   const eventType = evt.type;
 
-  if (eventType === "user.created") {
+  if (eventType == "user.created") {
     await db.user.create({
       data: {
         externalUserId: payload.data.id,
         username: payload.data.username,
         imageUrl: payload.data.image_url,
+        stream: {
+          create: {
+            name: `${payload.data.username}'s stream`,
+          },
+        },
       },
     });
   }
-
-  if (eventType === "user.updated") {
+  if (eventType == "user.updated") {
     const currentUser = await db.user.findUnique({
       where: {
         externalUserId: payload.data.id,
       },
     });
-
     if (!currentUser) {
       return new Response("User not found", { status: 404 });
     }
-
     await db.user.update({
       where: {
         externalUserId: payload.data.id,
@@ -75,8 +86,7 @@ export async function POST(req: Request) {
       },
     });
   }
-
-  if (eventType === "user.deleted") {
+  if (eventType == "user.deleted") {
     await db.user.delete({
       where: {
         externalUserId: payload.data.id,
